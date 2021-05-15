e27b16b94d6c767efbcca184b92e2e83adba278c
osydorchenko <ljubljana.sasha@gmail.com>
2021-05-15 14:26:52 +0300

    adding new class MainPageLocators with constant LOGIN_LINK

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 from .pages.main_page import MainPage
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,10 +14,4 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
