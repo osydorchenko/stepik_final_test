87f0a3c64d5f5c2c0e7873642512f46d702010ce
osydorchenko <ljubljana.sasha@gmail.com>
2021-05-15 15:44:24 +0300

    adding check for placement of login and register forms on login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,5 +14,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_page()
 
 
