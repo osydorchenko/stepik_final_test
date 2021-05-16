c7eb2673d7f8c6986202fa70090a0a51cdbd1215
osydorchenko <ljubljana.sasha@gmail.com>
2021-05-16 11:43:02 +0300

    adding parametrize for promo_url, mark 1 test with xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from .pages.product_page import PageObject
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+@pytest.mark.parametrize('promo_code', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, promo_code):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}'
     page = PageObject(browser, link)
     page.open()
     page.should_be_adding_basket_button()
