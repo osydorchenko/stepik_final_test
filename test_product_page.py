901282880b3a0772bc46daec5b47b5bdfbbc6548
osydorchenko <ljubljana.sasha@gmail.com>
2021-05-18 23:05:07 +0300

    improve test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -89,9 +89,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-    # login_page = LoginPage(browser, browser.current_url)
-    # login_page.should_be_login_page()
-    # login_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 @pytest.mark.need_review
